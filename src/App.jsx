291391db7930353361e95bafcffb578f0745b19b
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-09-27 12:08:16 -0300

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Post } from "./Post";
+import './global.css';
+import { Header } from "./components/Header/Header";
 
 export function App() {
 
   return (
     <div>
+
+    <Header/>
+
       <Post 
         author="Janis" 
         content="Kozmic blues"
-      />
-      <Post 
-        author="Keith" 
-        content="You got the silver"
       />
     </div>
     
